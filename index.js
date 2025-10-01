9813c1ab387ca1281e71e0e4a104052f0a4b3bbb
troncosojose22 <troncosojose22@gmail.com>
2025-10-01 12:25:55 -0400

    Second Function

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ const fiveToOneHundred = () => {
 }
 
 //fiveToOneHundred() tested(correct)
+
+const multiplesOfThree = () => {
+    for (let i = 3; i < 100; i +=3) {
+        console.log(i);
+    }
+}
+
+//multiplesOfThree();
