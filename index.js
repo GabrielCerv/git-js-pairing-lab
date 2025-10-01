e45a391a011e983b93bea48b5b28003b7c8f6d22
[Your Name] <[Your GitHub Email Address]>
2025-10-01 12:40:58 -0400

    Third Function

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ const fiveToOneHundred = () => {
 //fiveToOneHundred() tested(correct)
 
 const multiplesOfThree = () => {
-    for (let i = 3; i < 100; i +=3) {
+    for (let i = 3; i < 100; i += 3) {
         console.log(i);
     }
 }
 
 //multiplesOfThree();
+
+const multiplesOfThreeOrFive = () => {
+    for (let i = 3; i < 100; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            console.log(i)
+        }
+    }
+}
+// multiplesOfThreeOrFive();
